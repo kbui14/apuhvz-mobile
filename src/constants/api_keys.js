14c9db56c2313b75254453b7ff5bf789fd7c88fc
Kevin Bui <kevinbui@Kevins-MacBook-Pro.local>
2017-11-21 21:31:24 -0800

    Import Starter Pack

---
+++ src/constants/api_keys.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+// Keys to export (or use internally)
+
+// https://developers.google.com/places/web-service/
+export const GOOGLE_PLACES_API_KEY = "YOUR_KEY_HERE";
+
+// https://developers.facebook.com/apps/
+export const FACEBOOK_API_KEY = "YOUR_KEY_HERE";
+
+// https://console.firebase.google.com/
+export const GOOGLE_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBKnYOCbSABcHh-dilT0vPykacDvs08yqw",
+    authDomain: "apuhvz-test.firebaseapp.com",
+    databaseURL: "https://apuhvz-test.firebaseio.com",
+    projectId: "apuhvz-test",
+    storageBucket: "apuhvz-test.appspot.com",
+    messagingSenderId: "295324303884"
+}
---
+++ src/constants/style.js
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = '#3d434c';
+export const SECONDARY_COLOR = '#4c493c';
+export const SECONDARY_COLOR_LIGHT = '#e2dec9';
+export const SEAFOAM_COLOR = '#bbdfc6';
+export const DARK_GREEN = '#3d675e';
+export const GREEN = '#5da18a';
+export const LIGHT_GREEN = '#70c2a5';
